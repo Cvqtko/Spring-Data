63d8ff8146d4ae565542edceebf4245790cb3d7f
Cvqtko <cvqtkokirilov@gmail.com>
2020-06-05 07:31:06 +0300

    Introduction to hibernate

--- 06_introduction_to_hibernate_exercise/src/main/Engine.java
+++ 06_introduction_to_hibernate_exercise/src/main/Engine.java
@@ -60,6 +60,8 @@ public class Engine implements Runnable {
 		// Ex. 7
 		this.addressWithEmployeeCount();
 
+		// Ex. 7
+		this.addressWithEmployeeCount();
 	}
 
 	private void addressWithEmployeeCount() {
